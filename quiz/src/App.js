93c5e66c2abfc453532764047bb7f9f3c0f2d6b2
Alexsey779 <fktrctq321@gmail.com>
2024-04-14 15:47:25 +0700

    commit 2

--- quiz/src/App.js
+++ quiz/src/App.js
@@ -1,44 +1,51 @@
-import React from 'react'
+import React from "react";
 
-import './index.scss';
+import "./index.scss";
 
 const questions = [
   {
-    title: 'React - это ... ?',
-    variants: ['библиотека', 'фреймворк', 'приложение'],
+    title: "React - это ... ?",
+    variants: ["библиотека", "фреймворк", "приложение"],
     correct: 0,
   },
   {
-    title: 'Компонент - это ... ',
-    variants: ['приложение', 'часть приложения или страницы', 'то, что я не знаю что такое'],
+    title: "Компонент - это ... ",
+    variants: [
+      "приложение",
+      "часть приложения или страницы",
+      "то, что я не знаю что такое",
+    ],
     correct: 1,
   },
   {
-    title: 'Что такое JSX?',
+    title: "Что такое JSX?",
     variants: [
-      'Это простой HTML',
-      'Это функция',
-      'Это тот же HTML, но с возможностью выполнять JS-код',
+      "Это простой HTML",
+      "Это функция",
+      "Это тот же HTML, но с возможностью выполнять JS-код",
     ],
     correct: 2,
   },
 ];
 
-function Result({correct}) {
+function Result({ correct }) {
   return (
     <div className="result">
-      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" alt=''/>
-      <h2>Вы отгадали {correct} ответа из {questions.length}</h2>
-      <a href='/'>
+      <img
+        src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png"
+        alt=""
+      />
+      <h2>
+        Вы отгадали {correct} ответа из {questions.length}
+      </h2>
+      <a href="/">
         <button>Попробовать снова</button>
       </a>
-      
     </div>
   );
 }
 
-function Game({step, question, onClickVariant}) {
-   
+function Game({ step, question, onClickVariant }) {
   const percent = Math.round((step / questions.length) * 100);
   return (
     <>
@@ -47,13 +54,11 @@ function Game({step, question, onClickVariant}) {
       </div>
       <h1>{question.title}</h1>
       <ul>
-        {
-          question.variants.map((text, index )=> (
-            <li onClick = {() => onClickVariant(index)} key={text}>{text}</li>
-          ))
-        }
-        
-        
+        {question.variants.map((text, index) => (
+          <li onClick={() => onClickVariant(index)} key={text}>
+            {text}
+          </li>
+        ))}
       </ul>
     </>
   );
@@ -62,34 +67,23 @@ function Game({step, question, onClickVariant}) {
 function App() {
   const [step, setStep] = React.useState(0);
   const [correct, setCorrect] = React.useState(0);
-  
+
   const question = questions[step];
-  
- 
 
   const onClickVariant = (index) => {
+    setStep(step + 1);
 
-setStep(step + 1);
-
-if (index === question.correct) (
-  setCorrect(correct + 1)
-)
-
+    if (index === question.correct) setCorrect(correct + 1);
   };
   return (
     <div className="App">
-
-      { 
-      
-      step !== questions.length ? <Game  step={step} question={question} onClickVariant={onClickVariant}/> :  <Result correct={correct}/>  
-       
-        
-      }
-      
-     
+      {step !== questions.length ? (
+        <Game step={step} question={question} onClickVariant={onClickVariant} />
+      ) : (
+        <Result correct={correct} />
+      )}
     </div>
   );
 }
 
 export default App;
-
