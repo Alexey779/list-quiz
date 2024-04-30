9dea9764419372c917299ab8e3260082c165526e
Alexsey779 <fktrctq321@gmail.com>
2024-04-30 22:14:07 +0700

    commit 4

--- convector/src/App.js
+++ convector/src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Block } from './Block';
+import './index.scss';
 
 function App() {
+  //const [rates, setRates] = React.useState({});
+
+  const ratesRef = React.useRef({});
+  const [fromCurrency, setFromCurrency] = React.useState('AUD');
+
+  const [toCurrency, setToCurrency] = React.useState('USD');
+  const [fromPrice, setFromPrice] = React.useState(0);
+  const [toPrice, setToPrice] = React.useState(1);
+
+  React.useEffect(() => {
+    fetch('https://www.cbr-xml-daily.ru/latest.js')
+    .then((res) => res.json())
+    .then((json) => {
+     // setRates(json.rates);
+     ratesRef.current = json.rates; 
+     onChangeToPrice(1);
+    }) 
+    .catch((err) => {
+      console.warn(err);
+      alert('no info')
+    })
+  },[]);
+
+  
+
+  const onChangeFromPrice = (value) =>{
+    const price = value / ratesRef.current[fromCurrency];
+    const result = price * ratesRef.current[toCurrency]
+    setToPrice(result.toFixed(2));
+    setFromPrice(value);
+  }
+  
+  const onChangeToPrice = (value) =>{
+    const result = (ratesRef.current[fromCurrency] / ratesRef.current[toCurrency]) * value;
+    setFromPrice(result.toFixed(2));
+    setToPrice(value);
+  }
+    
+  React.useEffect(() => {
+onChangeFromPrice(fromPrice);
+
+  }, [fromCurrency]);
+
+  React.useEffect(() => {
+    onChangeToPrice(toPrice);
+    
+      }, [toCurrency]);
+    
   return (
+    <>
+    <h1 className='convector'>Convector</h1>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Block 
+      value={fromPrice}
+       currency={fromCurrency} 
+       onChangeCurrency={setFromCurrency} 
+       onChangeValue={onChangeFromPrice} 
+       />
+      <Block 
+      value={toPrice} 
+      currency={toCurrency} 
+      onChangeCurrency={setToCurrency}
+      onChangeValue={onChangeToPrice}
+      />
     </div>
-  );
+ </> );
 }
 
 export default App;
