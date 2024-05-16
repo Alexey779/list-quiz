487ef324f850bee372c52f57503aca71d14f94dc
Alexsey779 <fktrctq321@gmail.com>
2024-05-16 19:11:10 +0700

    first commit content

--- content/src/App.js
+++ content/src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import './index.scss';
+import  Collection  from './Collection';
 
+
+const cats = [
+  { "name": "Все" },
+  { "name": "Море" },
+  { "name": "Горы" },
+  { "name": "Архитектура" },
+  { "name": "Города" }
+]
 function App() {
+  const [categoryId, setCategoryId] = React.useState(0);
+  const [page, setPage] = React.useState(1);
+  const [isLoding, setIsLoding ] = React.useState(true);
+  const [serchValue, setSerchValue] = React.useState('');
+  const [collections, setCollection] = React.useState([]);
+
+  useEffect(() => {
+    setIsLoding(true);
+    const category = categoryId ? `category=${categoryId}` : ''
+fetch(`https://6635ccd8415f4e1a5e255d64.mockapi.io/collection?page=${page}&limit=3&${category}`)
+ .then ((res) => res.json()) 
+ .then ((json) => {
+  setCollection(json) 
+})
+ .catch((error) => {
+  console.warn(error);
+  alert('Error');
+ })
+ .finally(() => setIsLoding(false));
+  
+ }, [categoryId, page]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Моя коллекция фотографий</h1>
+      <div className="top">
+        <ul className="tags">
+          {cats.map((obj, i) => 
+          (<li 
+          onClick={() => setCategoryId(i)}
+          className={categoryId === i ? 'active' : '' } key={obj.name}>{obj.name}</li>))}
+        </ul>
+        <input value = {serchValue} onChange = {(e) => setSerchValue(e.target.value)} className="search-input" placeholder="Поиск по названию" />
+      </div>
+      <div className="content">
+        {isLoding ? <h1>Идет загрузка...</h1> :
+        (collections
+        .filter((obj) => {
+        return obj.name.toLowerCase().includes(serchValue.toLowerCase())})
+        .map((obj) => (
+        <Collection
+        key={obj.id}
+           name = {obj.name}
+          images={obj.photos} 
+         
+        />
+        )))
+        
+        }
+        
+      </div>
+      <ul className="pagination">
+        {
+          [...Array(5)].map((_, i) => (
+            <li onClick = {() => setPage(i+1)} className={page === i+1  ? 'active': ''}>{i+1}</li>
+          ))
+        }
+      </ul>
     </div>
   );
 }
